cdba919465b898f5305d2951d146bcdb9f3a6028
John Stein <john.stein.483@gmail.com>
2023-07-22 00:50:34 -0500

    added some button click changes.  still just testing git really

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+console.log('whatever')
+
+var searchTermElem = document.getElementById("searchTermButton");
+
+
+testFunction = function(){
+    console.log('whatever')
+};
+//searchTermElem.onclick(alert("WOW!"));
+
+searchTermElem.addEventListener("click", testFunction);
 
